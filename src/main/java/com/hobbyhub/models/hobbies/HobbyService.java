847b722d21ab66cf46f1033cd4a3a118249c15de
Samer <samerrawashdeh999@gmail.com>
2021-04-24 01:27:06 +0300

    build get hobby feed api

--- src/main/java/com/hobbyhub/models/hobbies/HobbyService.java
+++ src/main/java/com/hobbyhub/models/hobbies/HobbyService.java
@@ -1,13 +1,17 @@
 package com.hobbyhub.models.hobbies;
 
+import com.hobbyhub.models.posts.Post;
+import com.hobbyhub.models.posts.PostService;
+import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HobbyService {
-  @Autowired
-  private HobbyRepository hobbyRepository;
+  @Autowired private HobbyRepository hobbyRepository;
+
+  @Autowired private PostService postService;
 
   public Hobby addHobby(Hobby hobby) {
     if (hobby.getId() != null) {
@@ -36,5 +40,12 @@ public class HobbyService {
 
   public Hobby getHobbyByName(String hobbyName) {
     return hobbyRepository.getHobbyByName(hobbyName);
+  }
+
+  public List<Post> getHobbyFeed(String hobbyName) {
+    if (getHobbyByName(hobbyName) == null) {
+      throw new IllegalArgumentException(String.format("hobbyName[%s] doesn't exist", hobbyName));
+    }
+    return postService.getPostsByCategoriesContaining(Collections.singletonList(hobbyName));
   }
 }
